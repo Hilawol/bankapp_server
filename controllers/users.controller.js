91ec2d3761bac68772992242bd7af4f14cf965a4
Hila Kershenovich <hilawol@gmail.com>
2021-04-23 21:13:47 +0300

    added get user

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -18,11 +18,26 @@ const addUser = async (req, res) => {
     const result = await user.save();
     return res.status(201).json({ "success": result });
   } catch (error) {
-    return res.status(505).json({ "error": error })
+    return res.status(500).json({ "error": error })
+  }
+}
+
+//Gets user by identificatioh number (not the _id object)
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await userModel.findOne({ id: id });
+    if (!user) {
+      return res.status(404).json({ "error": "User not found" });
+    }
+    return res.send(user);
+  } catch (error) {
+    return res.status(500).json({ "error": error });
   }
 }
 
 module.exports = {
   getAllUsers,
-  addUser
+  addUser,
+  getUser
 }
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -6,6 +6,8 @@ router.get('/', (req, res) => {
   usersController.getAllUsers(req, res);
 }).post('/', (req, res) => {
   usersController.addUser(req, res);
+}).get('/:id', (req, res) => {
+  usersController.getUser(req, res);
 });
 
 module.exports = router;
