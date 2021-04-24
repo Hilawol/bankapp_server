3c86af9bfe4f677186870a64d1c08b893a7dc555
Hila Kershenovich <hilawol@gmail.com>
2021-04-25 02:28:42 +0300

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const usersRoute = require('./routes/users.routes');
@@ -9,6 +10,7 @@ const transactionsRoute = require('./routes/transaction.routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/bank/users', usersRoute);
 app.use('/bank/accounts', accountsRoute);
